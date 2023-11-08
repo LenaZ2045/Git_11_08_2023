714e046fbb0d10d5bbd40c9736b9f8847e4780b6
Lena Zimin <Bogypesn@gmail.com>
2023-11-08 15:52:47 -0500

    Created two changes 1. added text in read.me file 2. created GIT Java class

---
+++ Git11_08_2023.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Git11_08_2023 {
+    public static void main(String[] args) {
+        List<Integer> integer = new ArrayList<>();
+        integer.add(12);
+        integer.add(6);
+        integer.add(3);
+
+        System.out.println(integer);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Git_11_08_2023
+This is Read-Me Changes and Additions
